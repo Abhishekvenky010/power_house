cbeed5d770b215d89517127afe8e2cfe99b34d33
Abhishek010 <abhishekvenky010@gmail.com>
2026-07-29 21:59:38 +0530

    done assets calculate and errors

---
+++ my-dex/programs/my-dex/src/assets.rs
@@ -0,0 +1,275 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{self, *};
+
+use crate::{
+    error::{MarketError, TraderEntryError},
+    state::{Market, TraderEntry, TraderState},
+};
+
+pub fn transfer_from_user<'info>(
+    token_program: &Program<'info, Token>,
+    from: &Account<'info, TokenAccount>,
+    to: &Account<'info, TokenAccount>,
+    authority: &Signer<'info>,
+    amount: u64,
+) -> Result<()> {
+    token::transfer(
+        CpiContext::new(
+            token_program.to_account_info(),
+            Transfer {
+                from: from.to_account_info(),
+                to: to.to_account_info(),
+                authority: authority.to_account_info(),
+            },
+        ),
+        amount,
+    )
+}
+
+pub fn transfer_from_vault<'info>(
+    token_program: &Program<'info, Token>,
+    from: &Account<'info, TokenAccount>,
+    to: &Account<'info, TokenAccount>,
+    vault_signer: &AccountInfo<'info>,
+    seeds: &[&[&[u8]]],
+    amount: u64,
+) -> Result<()> {
+    token::transfer(
+        CpiContext::new_with_signer(
+            token_program.to_account_info(),
+            Transfer {
+                from: from.to_account_info(),
+                to: to.to_account_info(),
+                authority: vault_signer.to_account_info(),
+            },
+            seeds,
+        ),
+        amount,
+    )
+}
+
+pub fn lock_bid_funds<'info>(
+    market: &mut Market,
+    owner: &Signer<'info>,
+    user_base_vault: &Account<'info, TokenAccount>,
+    user_quote_vault: &Account<'info, TokenAccount>,
+    quote_vault: &Account<'info, TokenAccount>,
+    token_program: &Program<'info, Token>,
+    quote_lots: u64,
+    base_lots: u64,
+) -> Result<u64> {
+    let base_lot_size = market.base_lot_size;
+    let quote_lot_size = market.quote_lot_size;
+    let total_quote_lots = quote_lots
+        .checked_mul(base_lots)
+        .ok_or(MarketError::MathOverflow)?;
+    let amount_to_lock = total_quote_lots
+        .checked_mul(quote_lot_size)
+        .ok_or(MarketError::MathOverflow)?;
+
+    require!(
+        user_quote_vault.amount >= amount_to_lock,
+        MarketError::InsufficientQuoteBalance
+    );
+    transfer_from_user(
+        token_program,
+        user_quote_vault,
+        quote_vault,
+        owner,
+        amount_to_lock,
+    )?;
+
+    let existing_entry = market.get_trader_entry(&owner.key());
+    let updated_quote_balance = user_quote_vault
+        .amount
+        .checked_sub(amount_to_lock)
+        .ok_or(MarketError::UnderFlow)?
+        .checked_div(quote_lot_size)
+        .ok_or(MarketError::UnderFlow)?;
+    let current_base_lots = user_base_vault
+        .amount
+        .checked_div(base_lot_size)
+        .ok_or(MarketError::UnderFlow)?;
+
+    match existing_entry {
+        Some(entry) => {
+            entry.trader_state.quote_lots_locked = entry
+                .trader_state
+                .quote_lots_locked
+                .checked_add(total_quote_lots)
+                .ok_or(MarketError::MathOverflow)?;
+
+            entry.trader_state.quote_lots_free = updated_quote_balance;
+            entry.trader_state.base_lots_free = current_base_lots;
+        }
+        None => {
+            msg!("quote free:{}:", user_quote_vault.amount);
+            let trader_state = TraderState {
+                base_lots_free: current_base_lots,
+                base_lots_locked: 0,
+                quote_lots_locked: total_quote_lots,
+                quote_lots_free: updated_quote_balance,
+            };
+            let trader_entry = TraderEntry {
+                trader_key: owner.key(),
+                trader_state,
+            };
+            market.trader_entry.push(trader_entry);
+        }
+    }
+    msg!("Transfer successful");
+    Ok(amount_to_lock)
+}
+
+pub fn lock_ask_funds<'info>(
+    market: &mut Market,
+    owner: &Signer<'info>,
+    user_base_vault: &Account<'info, TokenAccount>,
+    base_vault: &Account<'info, TokenAccount>,
+    token_program: &Program<'info, Token>,
+    base_lots: u64,
+) -> Result<u64> {
+    msg!("=== lock_ask_funds ===");
+    msg!(
+        "base_lots={} base_lot_size={} vault_amount={}",
+        base_lots,
+        market.base_lot_size,
+        user_base_vault.amount
+    );
+
+    let base_lot_size = market.base_lot_size;
+
+    let amount_to_lock = base_lots
+        .checked_mul(base_lot_size)
+        .ok_or(MarketError::MathOverflow)?;
+
+    msg!("amount_to_lock={}", amount_to_lock);
+    require!(
+        user_base_vault.amount >= amount_to_lock,
+        MarketError::InsufficientBaseBalance
+    );
+    transfer_from_user(
+        token_program,
+        user_base_vault,
+        base_vault,
+        owner,
+        amount_to_lock,
+    )?;
+    let existing_entry = market.get_trader_entry(&owner.key());
+
+    let amount_to_update = user_base_vault
+        .amount
+        .checked_sub(amount_to_lock)
+        .ok_or(MarketError::UnderFlow)?;
+    msg!("amount_to_update={}", amount_to_update);
+    let free_lots = amount_to_update
+        .checked_div(base_lot_size)
+        .ok_or(MarketError::UnderFlow)?;
+    msg!("free_lots={}", free_lots);
+    match existing_entry {
+        Some(entry) => {
+            entry.trader_state.base_lots_locked = entry
+                .trader_state
+                .base_lots_locked
+                .checked_add(base_lots)
+                .ok_or(MarketError::MathOverflow)?;
+            // if you are not understanding these fields you should read phoenix orderbook crankerless design
+            entry.trader_state.base_lots_free = free_lots
+        }
+        None => {
+            msg!("base free:{}:", user_base_vault.amount);
+            let trader_state = TraderState {
+                base_lots_free: free_lots,
+                base_lots_locked: base_lots,
+                quote_lots_locked: 0,
+                quote_lots_free: 0,
+            };
+            let trader_entry = TraderEntry {
+                trader_key: owner.key(),
+                trader_state,
+            };
+            market.trader_entry.push(trader_entry);
+        }
+    }
+    Ok(amount_to_lock)
+}
+
+pub fn unlock_bid_funds<'info>(
+    market: &mut Market,
+    quote_lots: u64,
+    owner: &Pubkey,
+    taker_qty: u64,
+    user_quote_vault: &Account<'info, TokenAccount>,
+) -> Result<u64> {
+    let quote_lots_to_move = quote_lots
+        .checked_mul(taker_qty)
+        .ok_or(MarketError::MathOverflow)?;
+    let amount_to_move = quote_lots_to_move
+        .checked_mul(market.quote_lot_size)
+        .ok_or(MarketError::MathOverflow)?;
+
+    require!(amount_to_move > 0, MarketError::InvalidPrice);
+    require!(
+        user_quote_vault.amount >= amount_to_move,
+        MarketError::InsufficientQuoteBalance
+    );
+    let existing_entry = market.get_trader_entry(owner);
+
+    match existing_entry {
+        Some(entry) => {
+            entry.trader_state.quote_lots_locked = entry
+                .trader_state
+                .quote_lots_locked
+                .checked_sub(quote_lots_to_move)
+                .ok_or(MarketError::UnderFlow)?;
+
+            entry.trader_state.quote_lots_free = entry
+                .trader_state
+                .quote_lots_free
+                .checked_add(quote_lots_to_move)
+                .ok_or(MarketError::MathOverflow)?;
+        }
+        None => {
+            return err!(TraderEntryError::EntryNotFound);
+        }
+    }
+    Ok(amount_to_move)
+}
+
+pub fn unlock_ask_funds<'info>(
+    market: &mut Market,
+    taker_qty: u64,
+    owner: &Pubkey,
+    user_base_vault: &Account<'info, TokenAccount>,
+) -> Result<u64> {
+    let amount_to_move = taker_qty
+        .checked_mul(market.base_lot_size)
+        .ok_or(MarketError::MathOverflow)?;
+
+    require!(amount_to_move > 0, MarketError::InvalidBaseQty);
+    require!(
+        user_base_vault.amount >= amount_to_move,
+        MarketError::InsufficientBaseBalance
+    );
+
+    let existing_entry = market.get_trader_entry(owner);
+    match existing_entry {
+        Some(entry) => {
+            entry.trader_state.base_lots_locked = entry
+                .trader_state
+                .base_lots_locked
+                .checked_sub(taker_qty)
+                .ok_or(MarketError::UnderFlow)?;
+
+            entry.trader_state.base_lots_free = entry
+                .trader_state
+                .base_lots_free
+                .checked_add(taker_qty)
+                .ok_or(MarketError::MathOverflow)?;
+        }
+        None => {
+            return err!(TraderEntryError::EntryNotFound);
+        }
+    }
+    Ok(amount_to_move)
+}
---
+++ my-dex/programs/my-dex/src/errors.rs
@@ -0,0 +1,169 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum OrderError {
+    #[msg("Insufficient balance")]
+    BalanceError,
+    #[msg("Order book full!")]
+    OrderbookFull,
+    #[msg("order not found!")]
+    OrderNotFound,
+    #[msg("Orders exceeded!")]
+    OrderFull,
+    #[msg("No free space for the order!")]
+    NoSpace,
+    #[msg("Quantity underflow!")]
+    UnderFlow,
+    #[msg("Quantity Overflow!")]
+    OverFlow,
+    #[msg("Failed to get first order!")]
+    InvalidNode,
+    #[msg("Order can be match , So rejecting the order!")]
+    WouldMatchImmediately,
+    #[msg("Invalid Order type!")]
+    InvalidOrderType,
+}
+
+#[error_code]
+pub enum MarketError {
+    #[msg("Market is paused!")]
+    MarketActiveError,
+    #[msg("Max base size should be greater than minimum order size!")]
+    MarketOrderSizeError,
+    #[msg("Value overflowed!")]
+    MathOverflow,
+    #[msg("No orders")]
+    NoOrders,
+    #[msg("Order id overflow!")]
+    OrderIdOverFlow,
+    #[msg("insufficient balance!")]
+    InsufficientQuoteBalance,
+    #[msg("insufficient balance!")]
+    InsufficientBaseBalance,
+    #[msg("Invalid token owner!")]
+    InvalidTokenAccountOwner,
+    #[msg("Invalid token Mint!")]
+    InvalidTokenMint,
+    #[msg("Destination vault is uninitialise!")]
+    DestinationVaultUninitialized,
+    #[msg("Invalid market account!")]
+    InvalidMarketAccount,
+    #[msg("Invalid Quote Price!")]
+    InvalidPrice,
+    #[msg("Invalid base quantity!")]
+    InvalidBaseQty,
+    #[msg("Invalid taker token account!")]
+    InvalidTakerAccount,
+    #[msg("Invalid maker token account!")]
+    InvalidMakerAccount,
+    #[msg("Invalid open orders account!")]
+    InvalidOpenOrders,
+    #[msg("Invalid event queue account!")]
+    InvalidEventQueue,
+    #[msg("Invalid vault account!")]
+    InvalidVault,
+    #[msg("Event queue underflow!")]
+    EventQueueUnderflow,
+    #[msg("There is no events in Event queue!")]
+    EventQueueEmpty,
+    #[msg("Sequence over flow!")]
+    SeqOverflow,
+    #[msg("Maker order missing!")]
+    MissingMakerOpenOrder,
+    #[msg("Quantity is too low!")]
+    UnderFlow,
+}
+
+#[error_code]
+pub enum OpenOrderError {
+    #[msg("Order not found!")]
+    OrderNotFound,
+    #[msg("Order overflow error!")]
+    OrderOverFlow,
+    #[msg("Price should not be 0")]
+    PriceIsTooLow,
+    #[msg("Base quantity is too low!")]
+    UnderFlow,
+    #[msg("Base quantity is too large!")]
+    OverFlow,
+    #[msg("Invalid Maker open order!")]
+    InvalidMakerOpenOrder,
+}
+
+#[error_code]
+pub enum FillRecordError {
+    #[msg("Fill Record not found!")]
+    MissingFillRecord,
+}
+
+#[error_code]
+pub enum TraderEntryError {
+    #[msg("Trader entry not found!")]
+    EntryNotFound,
+}
+
+#[error_code]
+pub enum EventError {
+    #[msg("Order not found in event queue!")]
+    OrderNotFound,
+}
+
+#[error_code]
+pub enum SlabError {
+    #[msg("Order not found in the Slab!")]
+    OrderNotFound,
+    #[msg("Invalid quantity!")]
+    InvalidQty,
+    #[msg("Invalid price!")]
+    InvalidPrice,
+    #[msg("Duplicate order id!")]
+    DuplicateOrderId,
+    #[msg("Invalid inserting index!")]
+    InvalidIndex,
+    #[msg("Quantity underflow!")]
+    UnderFlow,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Unauthorized request!")]
+    UnAuthorized,
+    #[msg("removed order not matched with order from the slab!")]
+    MisMatchedOrder,
+    #[msg("Under flow error!")]
+    UnderFlow,
+    #[msg("Over flow error!")]
+    OverFlow,
+    #[msg("Invalid price!")]
+    InvalidPrice,
+}
+
+#[error_code]
+pub enum EventQueueError {
+    #[msg("Queue full!")]
+    QueueFull,
+    #[msg("Queue empty!")]
+    QueueEmpty,
+}
+
+#[error_code]
+pub enum ConsumeEventsError {
+    #[msg("remaining_accounts count must equal events_to_process * 4")]
+    InvalidRemainingAccountsCount,
+    #[msg("Taker base ATA does not match expected")]
+    InvalidTakerBaseAta,
+    #[msg("Taker quote ATA does not match expected")]
+    InvalidTakerQuoteAta,
+    #[msg("Maker base ATA does not match expected")]
+    InvalidMakerBaseAta,
+    #[msg("Maker quote ATA does not match expected")]
+    InvalidMakerQuoteAta,
+    #[msg("Math overflow in settlement calculation")]
+    MathOverflow,
+    #[msg("Fund can not be settle!")]
+    NonSettleableEvent,
+    #[msg("Invalid taker base ATA!")]
+    InvalidOwnerBaseAta,
+    #[msg("Invalid taker quote ATA!")]
+    InvalidOwnerQuoteAta,
+}
