4cb67305126f3bdf0cfdf5042dd5d43951ef258d
Abhishek010 <abhishekvenky010@gmail.com>
2026-08-10 20:42:15 +0530

    done hooks and providers

---
+++ frontend/hooks/useDexProgram.ts
@@ -0,0 +1,31 @@
+"use client"
+import { AnchorProvider, Program, Idl } from "@coral-xyz/anchor";
+
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import idl from "../idl/orderbook.json"
+import { Orderbook } from "../types/orderbook";
+import { useMemo } from "react";
+
+const useDexProgram = () => {
+    const { connection } = useConnection();
+    const wallet = useWallet();
+
+    const provider = useMemo(()=>{
+        if (!wallet.wallet) return null;
+        return new AnchorProvider(
+            connection,
+            wallet as any,
+            { commitment : "confirmed"}
+        );
+    },[[connection, wallet]]);
+
+    const program = useMemo(()=>{
+          if (!provider) return null;
+
+          return new Program<Orderbook>(
+            idl as Orderbook,
+            provider
+          );
+    },[provider]);
+    return { program, provider };
+}
